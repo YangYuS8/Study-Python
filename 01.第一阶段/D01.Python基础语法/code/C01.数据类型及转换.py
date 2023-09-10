1141963d41f0b76c4f3d390fab79bb4df7a84859
YangYuS8 <YangYuS8@163.com>
2023-09-10 14:58:19 +0800

    modified:   .vscode/Study-Python.code-workspace 	deleted:    "01.\347\254\254\344\270\200\351\230\266\346\256\265/D01.Python\345\237\272\347\241\200\350\257\255\346\263\225/code/C01.\346\225\260\346\215\256\347\261\273\345\236\213.py" 	new file:   "01.\347\254\254\344\270\200\351\230\266\346\256\265/D01.Python\345\237\272\347\241\200\350\257\255\346\263\225/code/C01.\346\225\260\346\215\256\347\261\273\345\236\213\345\217\212\350\275\254\346\215\242.py" 	new file:   "01.\347\254\254\344\270\200\351\230\266\346\256\265/D01.Python\345\237\272\347\241\200\350\257\255\346\263\225/code/C02.\346\240\207\350\257\206\347\254\246\344\270\216\350\277\220\347\256\227\347\254\246.py"

---
+++ 01.第一阶段/D01.Python基础语法/code/C01.数据类型及转换.py
@@ -0,0 +1,43 @@
+# 1.查询数据类型的方法：
+# 方法1：使用print直接输出类型信息
+print(type("黑马程序员"))
+print(type(666))
+print(type(114.514))
+
+# 方法2：使用变量存储type()语句的结果
+string_type = type("黑马程序员")
+int_type = type(666)
+float_type = type(114.514)
+print(string_type)
+print(int_type)
+print(float_type)
+
+# 方法3：使用type()语句，查看变量中存储的数据类型信息
+name = "黑马程序员"
+name_type = type(name)
+print(name_type)
+
+# 2.转换数据类型的方法：
+# 将数字类型转换成字符串
+num_str = str(11)
+print(type(num_str), num_str)
+float_str = str(114.514)
+print(type(float_str), float_str)
+
+# 将字符串转换成数字
+num = int("11")
+print(type(num), num)
+num2 = float("114.514")
+print(type(num2), num2)
+
+# 错误示例：想要将字符串转换成数字，必须要求字符串内的内容都是数字
+# num3 = int("黑马程序员")
+# print(type(num3), num3)
+
+# 将整数转换成浮点数
+float_num = float(11)
+print(type(float_num), float_num)
+
+# 将浮点数转换成整数
+int_num = int(114.514)
+print(type(int_num), int_num)
