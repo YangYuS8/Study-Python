f9bcf8e40986d34f35873b0f8a585b094f045ef8
YangYuS8 <YangYuS8@163.com>
2023-09-22 11:35:27 +0800

    modified:   "01.\347\254\254\344\270\200\351\230\266\346\256\265/D03.Python\345\207\275\346\225\260/code/C10.\345\207\275\346\225\260\347\232\204\350\257\264\346\230\216\346\226\207\346\241\243.py"

--- 01.第一阶段/D03.Python函数/code/C10.函数的说明文档.py
+++ 01.第一阶段/D03.Python函数/code/C10.函数的说明文档.py
@@ -1,1 +1,15 @@
 # 演示对函数进行文档说明
+# 定义函数并用文档说明
+def add(x, y):
+    """
+
+    Args:
+        x (_type_): _description_
+        y (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    result = x + y
+    print(f"两数相加的结果是:{result}")
+    return result
