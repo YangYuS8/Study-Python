71454e9aafda63b6dc190cbce378f005b2566588
YangYuS8 <YangYuS8@163.com>
2023-10-18 13:26:48 +0800

    modified:   "01.\347\254\254\344\270\200\351\230\266\346\256\265/D04.Python\346\225\260\346\215\256\345\256\271\345\231\250/code/C12.\345\210\227\350\241\250.py" 	modified:   "01.\347\254\254\344\270\200\351\230\266\346\256\265/D04.Python\346\225\260\346\215\256\345\256\271\345\231\250/xmind/D04.Python\346\225\260\346\215\256\345\256\271\345\231\250.xmind"

--- 01.第一阶段/D04.Python数据容器/code/C12.列表.py
+++ 01.第一阶段/D04.Python数据容器/code/C12.列表.py
@@ -3,7 +3,33 @@
 # 注意：这里的元素可以是列表，也就是嵌套
 
 # 定义一个列表list
+my_list = ["itheima", "itcast", "python"]
+print(my_list)
+print(type(my_list))
+
+my_list = ["itheima", 666, True]
+print(my_list)
+print(type(my_list))
 
 # 定义一个嵌套的列表
+my_list = [[1, 2, 3], [4, 5, 6]]
+print(my_list)
+print(type(my_list))
 
-#
+# 通过下标索引取出对应位置的数据
+my_list = ["Tom", "Lily", "Rose"]
+# 语法：列表[下标索引]，从前向后从0开始每次+1，从后向前从-1开始每次-1
+print(my_list[0])
+print(my_list[1])
+print(my_list[2])
+# 错误示范：通过下标索引数据时一定不要超出范围
+# print(my_list[3])
+
+# 通过下标索引取出对应位置的数据(倒序取出)
+print(my_list[-1])
+print(my_list[-2])
+print(my_list[-3])
+
+# 取出嵌套列表的元素
+my_list = [[1, 2, 3], [4, 5, 6]]
+print(my_list[1][1])
